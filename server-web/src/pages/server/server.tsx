1b133cdfdfe727275e0d40822215ada638b3148c
Zoowayss <shizuwei99@gmail.com>
2023-05-13 00:23:57 +0800

    feat: add web

---
+++ server-web/src/pages/server/server.tsx
@@ -0,0 +1,230 @@
+import React from 'react';
+import { Table } from 'antd';
+import 'antd/dist/antd.css';
+import { EllipsisOutlined, PlusOutlined } from '@ant-design/icons';
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
+import { ProTable, TableDropdown } from '@ant-design/pro-components';
+import { Button, Dropdown, Space, Tag } from 'antd';
+import { useRef } from 'react';
+import request from 'umi-request';
+
+type ServerItem = {
+  id: string;
+  domain: string,
+  ip4: string;
+  ip6: string;
+  password: string;
+  frpToken: string;
+  createdTime: string;
+  updatedTime: string;
+};
+
+
+const columns: ProColumns<ServerItem>[] = [
+  {
+    dataIndex: 'index',
+    valueType: 'indexBorder',
+    width: 48,
+  },
+  {
+    title: 'ip4',
+    dataIndex: 'ip4',
+    copyable: true,
+    ellipsis: true,
+    tip: '标题过长会自动收缩',
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
+  },
+  {
+    title: '密码',
+    dataIndex: 'password',
+    copyable: true,
+    ellipsis: true,
+    // tip: '标题过长会自动收缩',
+    // formItemProps: {
+    //   rules: [
+    //     {
+    //       required: true,
+    //       message: '此项为必填项',
+    //     },
+    //   ],
+    // },
+  },
+  {
+    title: 'ip6',
+    dataIndex: 'ip6',
+    copyable: true,
+    ellipsis: true,
+    tip: '标题过长会自动收缩',
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
+  },
+  {
+    title: 'domain',
+    dataIndex: 'domain',
+    copyable: true,
+    ellipsis: true,
+    tip: '标题过长会自动收缩',
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
+  },
+  {
+    title: '创建时间',
+    key: 'showTime',
+    dataIndex: 'createTime',
+    valueType: 'dateTime',
+    sorter: true,
+    hideInSearch: true,
+  },
+  {
+    title: '过期时间',
+    dataIndex: 'expireTime',
+    valueType: 'dateTime',
+    // hideInTable: true,
+    search: {
+      transform: (value) => {
+        return {
+          startTime: value[0],
+          endTime: value[1],
+        };
+      },
+    },
+  },
+  {
+    title: '操作',
+    valueType: 'option',
+    key: 'option',
+    render: (text, record, _, action) => [
+      <a
+        key="editable"
+        onClick={() => {
+          action?.startEditable?.(record.id);
+        }}
+      >
+        编辑
+      </a>,
+      <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
+        查看
+      </a>,
+      <TableDropdown
+        key="actionGroup"
+        onSelect={() => action?.reload()}
+        menus={[
+          { key: 'copy', name: '复制' },
+          { key: 'delete', name: '删除' },
+        ]}
+      />,
+    ],
+  },
+];
+
+export default () => {
+  const actionRef = useRef<ActionType>();
+  return (
+    <ProTable<ServerItem>
+      columns={columns}
+      actionRef={actionRef}
+      cardBordered
+      request={async (params = {}, sort, filter) => {
+        console.log(sort, filter);
+        return request<{
+          data: ServerItem[];
+        }>('/api/admin/server', {
+          params,
+        }).then((res) => {
+          return {data:res.data.records};
+        });
+      }}
+      editable={{
+        type: 'multiple',
+      }}
+      columnsState={{
+        persistenceKey: 'pro-table-singe-demos',
+        persistenceType: 'localStorage',
+        onChange(value) {
+          console.log('value: ', value);
+        },
+      }}
+      rowKey="id"
+      search={{
+        labelWidth: 'auto',
+      }}
+      options={{
+        setting: {
+          listsHeight: 400,
+        },
+      }}
+      form={{
+        // 由于配置了 transform，提交的参与与定义的不同这里需要转化一下
+        syncToUrl: (values, type) => {
+          if (type === 'get') {
+            return {
+              ...values,
+              created_at: [values.startTime, values.endTime],
+            };
+          }
+          return values;
+        },
+      }}
+      pagination={{
+        pageSize: 5,
+        onChange: (page) => console.log(page),
+      }}
+      dateFormatter="string"
+      headerTitle="高级表格"
+      toolBarRender={() => [
+        <Button
+          key="button"
+          icon={<PlusOutlined />}
+          onClick={() => {
+            actionRef.current?.reload();
+          }}
+          type="primary"
+        >
+          新建
+        </Button>,
+        <Dropdown
+          key="menu"
+          menu={{
+            items: [
+              {
+                label: '1st item',
+                key: '1',
+              },
+              {
+                label: '2nd item',
+                key: '1',
+              },
+              {
+                label: '3rd item',
+                key: '1',
+              },
+            ],
+          }}
+        >
+          <Button>
+            <EllipsisOutlined />
+          </Button>
+        </Dropdown>,
+      ]}
+    />
+  );
+};
